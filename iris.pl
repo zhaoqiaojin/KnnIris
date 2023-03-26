3062325f347d1bd79e9d066eca03b0ebe6e183cc
Joy <zhaoqiaojin@hotmail.com>
2023-03-25 19:19:59 -0700

    debugged everything
    currently works, prints hte predicted class and also a list of the k neighbors along with the distance formatted to 3 decimal places

--- iris.pl
+++ iris.pl
@@ -1,50 +1,198 @@
-% Load the Iris dataset
-load_iris :-
-    csv_read_file('iris.csv', [row(_,_,_,_,_)|Data], [functor(row)]),
-    assert_iris(Data).
+% Iris dataset
+data(5.1,3.5,1.4,0.2,'Iris-setosa').
+data(4.9,3.0,1.4,0.2,'Iris-setosa').
+data(4.7,3.2,1.3,0.2,'Iris-setosa').
+data(4.6,3.1,1.5,0.2,'Iris-setosa').
+data(5.0,3.6,1.4,0.2,'Iris-setosa').
+data(5.4,3.9,1.7,0.4,'Iris-setosa').
+data(4.6,3.4,1.4,0.3,'Iris-setosa').
+data(5.0,3.4,1.5,0.2,'Iris-setosa').
+data(4.4,2.9,1.4,0.2,'Iris-setosa').
+data(4.9,3.1,1.5,0.1,'Iris-setosa').
+data(5.4,3.7,1.5,0.2,'Iris-setosa').
+data(4.8,3.4,1.6,0.2,'Iris-setosa').
+data(4.8,3.0,1.4,0.1,'Iris-setosa').
+data(4.3,3.0,1.1,0.1,'Iris-setosa').
+data(5.8,4.0,1.2,0.2,'Iris-setosa').
+data(5.7,4.4,1.5,0.4,'Iris-setosa').
+data(5.4,3.9,1.3,0.4,'Iris-setosa').
+data(5.1,3.5,1.4,0.3,'Iris-setosa').
+data(5.7,3.8,1.7,0.3,'Iris-setosa').
+data(5.1,3.8,1.5,0.3,'Iris-setosa').
+data(5.4,3.4,1.7,0.2,'Iris-setosa').
+data(5.1,3.7,1.5,0.4,'Iris-setosa').
+data(4.6,3.6,1.0,0.2,'Iris-setosa').
+data(5.1,3.3,1.7,0.5,'Iris-setosa').
+data(4.8,3.4,1.9,0.2,'Iris-setosa').
+data(5.0,3.0,1.6,0.2,'Iris-setosa').
+data(5.0,3.4,1.6,0.4,'Iris-setosa').
+data(5.2,3.5,1.5,0.2,'Iris-setosa').
+data(5.2,3.4,1.4,0.2,'Iris-setosa').
+data(4.7,3.2,1.6,0.2,'Iris-setosa').
+data(4.8,3.1,1.6,0.2,'Iris-setosa').
+data(5.4,3.4,1.5,0.4,'Iris-setosa').
+data(5.2,4.1,1.5,0.1,'Iris-setosa').
+data(5.5,4.2,1.4,0.2,'Iris-setosa').
+data(4.9,3.1,1.5,0.1,'Iris-setosa').
+data(5.0,3.2,1.2,0.2,'Iris-setosa').
+data(5.5,3.5,1.3,0.2,'Iris-setosa').
+data(4.9,3.1,1.5,0.1,'Iris-setosa').
+data(4.4,3.0,1.3,0.2,'Iris-setosa').
+data(5.1,3.4,1.5,0.2,'Iris-setosa').
+data(5.0,3.5,1.3,0.3,'Iris-setosa').
+data(4.5,2.3,1.3,0.3,'Iris-setosa').
+data(4.4,3.2,1.3,0.2,'Iris-setosa').
+data(5.0,3.5,1.6,0.6,'Iris-setosa').
+data(5.1,3.8,1.9,0.4,'Iris-setosa').
+data(4.8,3.0,1.4,0.3,'Iris-setosa').
+data(5.1,3.8,1.6,0.2,'Iris-setosa').
+data(4.6,3.2,1.4,0.2,'Iris-setosa').
+data(5.3,3.7,1.5,0.2,'Iris-setosa').
+data(5.0,3.3,1.4,0.2,'Iris-setosa').
+data(7.0,3.2,4.7,1.4,'Iris-versicolor').
+data(6.4,3.2,4.5,1.5,'Iris-versicolor').
+data(6.9,3.1,4.9,1.5,'Iris-versicolor').
+data(5.5,2.3,4.0,1.3,'Iris-versicolor').
+data(6.5,2.8,4.6,1.5,'Iris-versicolor').
+data(5.7,2.8,4.5,1.3,'Iris-versicolor').
+data(6.3,3.3,4.7,1.6,'Iris-versicolor').
+data(4.9,2.4,3.3,1.0,'Iris-versicolor').
+data(6.6,2.9,4.6,1.3,'Iris-versicolor').
+data(5.2,2.7,3.9,1.4,'Iris-versicolor').
+data(5.0,2.0,3.5,1.0,'Iris-versicolor').
+data(5.9,3.0,4.2,1.5,'Iris-versicolor').
+data(6.0,2.2,4.0,1.0,'Iris-versicolor').
+data(6.1,2.9,4.7,1.4,'Iris-versicolor').
+data(5.6,2.9,3.6,1.3,'Iris-versicolor').
+data(6.7,3.1,4.4,1.4,'Iris-versicolor').
+data(5.6,3.0,4.5,1.5,'Iris-versicolor').
+data(5.8,2.7,4.1,1.0,'Iris-versicolor').
+data(6.2,2.2,4.5,1.5,'Iris-versicolor').
+data(5.6,2.5,3.9,1.1,'Iris-versicolor').
+data(5.9,3.2,4.8,1.8,'Iris-versicolor').
+data(6.1,2.8,4.0,1.3,'Iris-versicolor').
+data(6.3,2.5,4.9,1.5,'Iris-versicolor').
+data(6.1,2.8,4.7,1.2,'Iris-versicolor').
+data(6.4,2.9,4.3,1.3,'Iris-versicolor').
+data(6.6,3.0,4.4,1.4,'Iris-versicolor').
+data(6.8,2.8,4.8,1.4,'Iris-versicolor').
+data(6.7,3.0,5.0,1.7,'Iris-versicolor').
+data(6.0,2.9,4.5,1.5,'Iris-versicolor').
+data(5.7,2.6,3.5,1.0,'Iris-versicolor').
+data(5.5,2.4,3.8,1.1,'Iris-versicolor').
+data(5.5,2.4,3.7,1.0,'Iris-versicolor').
+data(5.8,2.7,3.9,1.2,'Iris-versicolor').
+data(6.0,2.7,5.1,1.6,'Iris-versicolor').
+data(5.4,3.0,4.5,1.5,'Iris-versicolor').
+data(6.0,3.4,4.5,1.6,'Iris-versicolor').
+data(6.7,3.1,4.7,1.5,'Iris-versicolor').
+data(6.3,2.3,4.4,1.3,'Iris-versicolor').
+data(5.6,3.0,4.1,1.3,'Iris-versicolor').
+data(5.5,2.5,4.0,1.3,'Iris-versicolor').
+data(5.5,2.6,4.4,1.2,'Iris-versicolor').
+data(6.1,3.0,4.6,1.4,'Iris-versicolor').
+data(5.8,2.6,4.0,1.2,'Iris-versicolor').
+data(5.0,2.3,3.3,1.0,'Iris-versicolor').
+data(5.6,2.7,4.2,1.3,'Iris-versicolor').
+data(5.7,3.0,4.2,1.2,'Iris-versicolor').
+data(5.7,2.9,4.2,1.3,'Iris-versicolor').
+data(6.2,2.9,4.3,1.3,'Iris-versicolor').
+data(5.1,2.5,3.0,1.1,'Iris-versicolor').
+data(5.7,2.8,4.1,1.3,'Iris-versicolor').
+data(6.3,3.3,6.0,2.5,'Iris-virginica').
+data(5.8,2.7,5.1,1.9,'Iris-virginica').
+data(7.1,3.0,5.9,2.1,'Iris-virginica').
+data(6.3,2.9,5.6,1.8,'Iris-virginica').
+data(6.5,3.0,5.8,2.2,'Iris-virginica').
+data(7.6,3.0,6.6,2.1,'Iris-virginica').
+data(4.9,2.5,4.5,1.7,'Iris-virginica').
+data(7.3,2.9,6.3,1.8,'Iris-virginica').
+data(6.7,2.5,5.8,1.8,'Iris-virginica').
+data(7.2,3.6,6.1,2.5,'Iris-virginica').
+data(6.5,3.2,5.1,2.0,'Iris-virginica').
+data(6.4,2.7,5.3,1.9,'Iris-virginica').
+data(6.8,3.0,5.5,2.1,'Iris-virginica').
+data(5.7,2.5,5.0,2.0,'Iris-virginica').
+data(5.8,2.8,5.1,2.4,'Iris-virginica').
+data(6.4,3.2,5.3,2.3,'Iris-virginica').
+data(6.5,3.0,5.5,1.8,'Iris-virginica').
+data(7.7,3.8,6.7,2.2,'Iris-virginica').
+data(7.7,2.6,6.9,2.3,'Iris-virginica').
+data(6.0,2.2,5.0,1.5,'Iris-virginica').
+data(6.9,3.2,5.7,2.3,'Iris-virginica').
+data(5.6,2.8,4.9,2.0,'Iris-virginica').
+data(7.7,2.8,6.7,2.0,'Iris-virginica').
+data(6.3,2.7,4.9,1.8,'Iris-virginica').
+data(6.7,3.3,5.7,2.1,'Iris-virginica').
+data(7.2,3.2,6.0,1.8,'Iris-virginica').
+data(6.2,2.8,4.8,1.8,'Iris-virginica').
+data(6.1,3.0,4.9,1.8,'Iris-virginica').
+data(6.4,2.8,5.6,2.1,'Iris-virginica').
+data(7.2,3.0,5.8,1.6,'Iris-virginica').
+data(7.4,2.8,6.1,1.9,'Iris-virginica').
+data(7.9,3.8,6.4,2.0,'Iris-virginica').
+data(6.4,2.8,5.6,2.2,'Iris-virginica').
+data(6.3,2.8,5.1,1.5,'Iris-virginica').
+data(6.1,2.6,5.6,1.4,'Iris-virginica').
+data(7.7,3.0,6.1,2.3,'Iris-virginica').
+data(6.3,3.4,5.6,2.4,'Iris-virginica').
+data(6.4,3.1,5.5,1.8,'Iris-virginica').
+data(6.0,3.0,4.8,1.8,'Iris-virginica').
+data(6.9,3.1,5.4,2.1,'Iris-virginica').
+data(6.7,3.1,5.6,2.4,'Iris-virginica').
+data(6.9,3.1,5.1,2.3,'Iris-virginica').
+data(5.8,2.7,5.1,1.9,'Iris-virginica').
+data(6.8,3.2,5.9,2.3,'Iris-virginica').
+data(6.7,3.3,5.7,2.5,'Iris-virginica').
+data(6.7,3.0,5.2,2.3,'Iris-virginica').
+data(6.3,2.5,5.0,1.9,'Iris-virginica').
+data(6.5,3.0,5.2,2.0,'Iris-virginica').
+data(6.2,3.4,5.4,2.3,'Iris-virginica').
+data(5.9,3.0,5.1,1.8,'Iris-virginica').
+% Define the distance between two instances
+distance(X1,Y1,Z1,W1,X2,Y2,Z2,W2,Dist) :-
+    Dist is sqrt((X1-X2)**2 + (Y1-Y2)**2 + (Z1-Z2)**2 + (W1-W2)**2).
 
-% Assert the Iris dataset as facts
-assert_iris([]).
-assert_iris([row(SepalLength, SepalWidth, PetalLength, PetalWidth, Species)|Rest]) :-
-    assert(iris(SepalLength, SepalWidth, PetalLength, PetalWidth, Species)),
-    assert_iris(Rest).
+% Define the KNN algorithm
+knn(K,X,Y,Z,W,Class,Neighbors) :-
+    findall(Distance-Class1, (data(X1,Y1,Z1,W1,Class1), distance(X,Y,Z,W,X1,Y1,Z1,W1,Distance)), Distances),
+    sort(Distances, SortedDistances),
+    take(K, SortedDistances, Nearest),
+    get_neighbors(Nearest, Neighbors),
+    get_classes(Neighbors, Classes),
+    classify(Classes, Class).
 
-% Compute the distance between two Iris samples
-distance(Sample1, Sample2, Distance) :-
-    iris(X1, Y1, Z1, W1, Sample1),
-    iris(X2, Y2, Z2, W2, Sample2),
-    Distance is sqrt((X1-X2)^2 + (Y1-Y2)^2 + (Z1-Z2)^2 + (W1-W2)^2).
+% Helper function to take the first K elements of a list
+take(0, _, []) :- !.
+take(_, [], []) :- !.
+take(K, [H|T], [H|R]) :- K1 is K-1, take(K1, T, R).
 
-% Find the k nearest neighbors of a given Iris sample
-k_nearest_neighbors(Sample, K, Neighbors) :-
-    findall(Distance-Species, (iris(_, _, _, _, Species), distance(Sample, Species, Distance)), Distances),
-    keysort(Distances, Sorted),
-    take(K, Sorted, Neighbors),
-    write('k_nearest_neighbors: '), write(Neighbors), nl.
+% Helper function to extract the neighbors with their distances
+get_neighbors([], []) :- !.
+get_neighbors([Distance-Class1|T], [FormattedDistance-Class1|R]) :-
+    format(atom(FormattedDistance), '~3f', [Distance]), % format distance to 3 decimal places
+    get_neighbors(T, R).
 
-% Take the first N elements of a list
-take(0, _, []).
-take(N, [X|Xs], [X|Ys]) :- N > 0, M is N-1, take(M, Xs, Ys).
+% Helper function to extract the class labels from the neighbors
+get_classes([], []) :- !.
+get_classes([_-C|T], [C|R]) :- get_classes(T, R).
 
-% Classify an Iris sample using the k-NN algorithm
-classify(Sample, K, Class) :-
-    k_nearest_neighbors(Sample, K, Neighbors),
-    count_neighbors(Neighbors, Counts),
-    keysort(Counts, Sorted),
-    reverse(Sorted, [Class-_|_]),
-    format('Predicted class: ~w~n', [Class]).
+% Define the classify function to return the most frequent class in the neighbors
+classify(Neighbors, Class) :-
+    sort(Neighbors, SortedNeighbors),
+    reverse(SortedNeighbors, ReverseNeighbors),
+    get_most_frequent(ReverseNeighbors, Class).
 
-% Count the occurrences of each species in a list of neighbors
-count_neighbors([], []).
-count_neighbors([_-Species|Neighbors], [(Count-Species)|Counts]) :-
-    count_species(Species, Neighbors, Count),
-    count_neighbors(Neighbors, Counts),
-    write('count_neighbors: '), write(Counts), nl.
+% Helper function to get the most frequent element in a list
+get_most_frequent([H|T], H) :- count_occurrences(H, [H|T], Count), \+ (member(X, T), count_occurrences(X, [H|T], Count1), Count1 > Count).
+get_most_frequent([H|T], R) :- get_most_frequent(T, R), count_occurrences(R, [H|T], CountR), count_occurrences(H, [H|T], CountH), CountR > CountH.
 
-% Count the occurrences of a species in a list of neighbors
-count_species(Species, Neighbors, Count) :-
-    include(=(_-Species), Neighbors, Matches),
-    length(Matches, Count).
+% Helper function to count the occurrences of an element in a list
+count_occurrences(_, [], 0).
+count_occurrences(X, [X|T], N) :- count_occurrences(X, T, N1), N is N1 + 1.
+count_occurrences(X, [Y|T], N) :- X \= Y, count_occurrences(X, T, N). 
+
+
 
 
 % :- <- library("ggplot2").
